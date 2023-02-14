d9bded54f40a67cda5867946d4a2628a2d6de74f
Cem Tuna Koç <113897119+CemTunaKoc@users.noreply.github.com>
2023-02-15 00:54:30 +0300

    when country selected country code is being set syncronously

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 import store from "./store";
 import { setUser } from "./store/auth";
-
+import country from './utils/country'
 export const userHandle = (data) => {
   store.dispatch(setUser(data));
 };
+export const getCountryCode = (countryCode) => country.find(ct => ct.code === countryCode).dial_code
