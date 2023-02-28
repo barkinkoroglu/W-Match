17ed3f890cbda09861523335a5fbe744b8d8b3df
Barkin Koroglu <barkin_45_99@hotmail.com>
2023-02-28 12:46:09 +0300

    Update test section

--- src/pages/test.js
+++ src/pages/test.js
@@ -4,9 +4,12 @@ import Data2 from "../questions/examquestion.json";
 import { useNavigate, useParams } from "react-router-dom";
 import { useSelector } from "react-redux";
 import { updateExam } from "../firebase";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 function Test() {
   const [data, setData] = useState([]);
   const [score, setScore] = useState(0);
+  const [disButton, setdisButton] = useState(false);
+  const [cquestion, setCquestion] = useState(0);
   const [examtime, setExamtime] = useState(500);
   const timerId = useRef();
   const navigate = useNavigate();
@@ -46,35 +49,49 @@ function Test() {
     navigate(`/home`);
   };
 
+  const handleCurrentIndex = () => {
+    setCquestion(cquestion + 1);
+    if (Data2.length === cquestion + 2) {
+      setdisButton(true);
+    }
+  };
+
   return (
-    <div className="min-h-screenh-screen   bg-gray-100 relative ">
-      <div className="fixed right-4 top-4 bg-slate-300 px-4 py-3 rounded-full font-medium text-lg text-center  w-24">
+    <div className="h-screen   bg-gray-200 relative flex flex-col  items-center gap-y-3   ">
+      <div className="absolute right-4 top-1 bg-slate-300 px-4 py-2 rounded-full font-medium text-lg text-center z-10  w-24">
         {examtime}
       </div>
       <div className="fixed right-4 top-1/2 bg-slate-300 px-6 py-5 rounded-full font-medium text-lg">
         {score}
       </div>
-      <div className="flex max-w-6xl px-3 py-5 mx-auto bg-white flex-col gap-y-4   ">
-        {Data2.map((record, index) => {
-          return (
-            <TestElement
-              key={index}
-              record={record}
-              index={index}
-              score={score}
-              setScore={setScore}
-            />
-          );
-        })}
-      </div>
-      <div className="flex max-w-6xl px-3 py-5 mx-auto bg-white justify-end">
-        <button
-          onClick={() => handleSubmit()}
-          className=" p-2 px-3 bg-slate-200 rounded-full  text-lg text-gray-800 hover:bg-slate-400"
-        >
-          {" "}
-          Submit the answer
-        </button>
+      <h1 className="text-3xl font-semibold">
+        {cquestion + 1} / {Data2.length}
+      </h1>
+      <div className="max-w-2xl mx-auto w-full shadow-md relative">
+        <div className="flex rounded-lg px-3 py-5  bg-white flex-col gap-y-4    ">
+          <TestElement
+            index={cquestion}
+            record={Data2[cquestion]}
+            score={score}
+            setScore={setScore}
+          />
+        </div>
+        <div className="flex  px-3 py-5 mx-auto bg-white justify-center ">
+          <button
+            className="p-2 bg-slate-300 hover:bg-slate-400 rounded-full disabled:opacity-40 disabled:hover:bg-slate-300"
+            onClick={() => handleCurrentIndex()}
+            disabled={disButton}
+          >
+            <ArrowForwardIosIcon />{" "}
+          </button>
+          <button
+            onClick={() => handleSubmit()}
+            className=" p-2 px-3 bg-slate-200 rounded-full absolute right-0 bottom-0 my-5 mx-3  text-lg text-gray-800 hover:bg-slate-400 "
+          >
+            {" "}
+            Submit the answer
+          </button>
+        </div>
       </div>
     </div>
   );
