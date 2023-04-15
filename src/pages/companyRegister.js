a75fbf941c103bac35d370298019d6daae56a86a
Cem Tuna Koç <60324303+CemTunna@users.noreply.github.com>
2023-04-15 16:37:24 +0300

    text fixed

--- src/pages/companyRegister.js
+++ src/pages/companyRegister.js
@@ -233,16 +233,16 @@ function CompanyRegister() {
             )}
           </Formik>
           <div className='text-center text-sm text-grey-dark mt-4'>
-            By signing up, you agree to the
+            By signing up, you agree to the{' '}
             <a
-              className='no-underline border-b border-grey-dark text-grey-dark'
+              className='no-underline border-b border-grey-dark text-grey-dark text-blue-600'
               href='#'
             >
-              Terms of Service
-            </a>{' '}
-            and
+              Terms of Service{' '}
+            </a>
+            and{' '}
             <a
-              className='no-underline border-b border-grey-dark text-grey-dark'
+              className='no-underline border-b border-grey-dark text-grey-dark text-blue-600'
               href='#'
             >
               Privacy Policy
--- src/pages/userRegister.js
+++ src/pages/userRegister.js
@@ -64,6 +64,8 @@ function UserRegister() {
       setPhoneCodeCountry(country.value.toLowerCase());
     }
   }, [country]);
+  console.log('c', country);
+  console.log('phone', tNumber);
   return (
     <div className='bg-grey-lighter h-full flex flex-col relative'>
       <img
@@ -290,16 +292,16 @@ function UserRegister() {
             )}
           </Formik>
           <div className='text-center text-sm text-grey-dark mt-4'>
-            By signing up, you agree to the
+            By signing up, you agree to the{' '}
             <a
-              className='no-underline border-b border-grey-dark text-grey-dark'
+              className='no-underline border-b border-grey-dark text-grey-dark text-blue-600'
               href='#'
             >
-              Terms of Service
-            </a>{' '}
-            and
+              Terms of Service{' '}
+            </a>
+            and{' '}
             <a
-              className='no-underline border-b border-grey-dark text-grey-dark'
+              className='no-underline border-b border-grey-dark text-grey-dark text-blue-600'
               href='#'
             >
               Privacy Policy
