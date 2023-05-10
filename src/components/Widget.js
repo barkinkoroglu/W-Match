251e9fd5e47f14027a6917b0b5d5b88a12ef503b
Barkin Koroglu <barkin_45_99@hotmail.com>
2023-05-10 23:49:55 +0300

    Updating Widget

--- src/components/Widget.js
+++ src/components/Widget.js
@@ -10,7 +10,17 @@ function Widget() {
     (async () => {
       await getRandomCompany(user.username).then((results) => {
         if (results) {
-          setCompanies(results);
+          let sortedNames = results.sort((a, b) => {
+            if (a.companyname < b.companyname) {
+              return -1;
+            }
+            if (a.companyname > b.companyname) {
+              return 1;
+            }
+            return 0;
+          });
+          setCompanies(sortedNames);
+          console.log("Sıralı", sortedNames);
         }
       });
     })();
@@ -19,11 +29,11 @@ function Widget() {
   return (
     <div className="flex-[0.3]  mx-12 mb-6 md:mx-0 md:mb-0">
       {user.type === 1 && (
-        <div className="flex flex-col  z-40 p-2 gap-y-2 rounded-lg shadow-lg bg-white">
+        <div className="flex flex-col h-1/2 overflow-y-auto scrollbar-hide  z-40 p-2 gap-y-2 rounded-lg shadow-lg bg-white">
           <h1 className=" border-b-2 p-2 w-full text-center font-medium border-gray-100">
             Add to your feed
           </h1>
-          {companies?.slice(0, 3).map((widg, index) => {
+          {companies?.slice(0, 10).map((widg, index) => {
             return <WidgetElement key={index} widg={widg} />;
           })}
         </div>
