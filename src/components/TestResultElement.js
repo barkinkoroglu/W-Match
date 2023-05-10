42b3bfe8bad68400df7472a84ad11a99a5c13887
Barkin Koroglu <barkin_45_99@hotmail.com>
2023-05-10 14:26:37 +0300

    Test Score Element Fixed

--- src/components/TestResultElement.js
+++ src/components/TestResultElement.js
@@ -1,8 +1,8 @@
-import { Avatar } from '@mui/material';
-import React from 'react';
-import { useEffect } from 'react';
-import { useState } from 'react';
-import { getUserInfobyID, getWmatchTests } from '../firebase';
+import { Avatar } from "@mui/material";
+import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+import { getUserInfobyID, getWmatchTests } from "../firebase";
 
 function TestResultElement(prop) {
   const [data, setData] = useState({});
@@ -13,29 +13,30 @@ function TestResultElement(prop) {
   useEffect(() => {
     getData();
   }, []);
-  useEffect(() => {
-    const fetchData = async () => {
-      const testsData = await getWmatchTests(prop.data.userid);
-      const point = +Object.values(testsData).toString();
-      setSum(Math.floor((point + prop.data.score) / 2));
-    };
-    fetchData();
-  }, []);
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     const testsData = await getWmatchTests(prop.data.userid);
+  //     const point = +Object.values(testsData).toString();
+  //     setSum(Math.floor((point + prop.data.score) / 2));
+  //   };
+  //   fetchData();
+  // }, []);
 
   if (data === null) {
     return <div>Loading</div>;
   }
 
   return (
-    <div className='flex gap-x-2 border-b-2 pb-3 border-gray-100'>
+    <div className="flex gap-x-2 border-b-2 pb-3 border-gray-100">
       <div>
         <Avatar src={data.ProfileUrl} />
       </div>
-      <div className='flex items-center justify-between w-full pr-6 '>
-        <a href={`/profile/${data?.username}`} className=' hover:underline '>
+      <div className="flex items-center justify-between w-full pr-6 ">
+        <a href={`/profile/${data?.username}`} className=" hover:underline ">
           {data?.name} {data?.lastname}
         </a>
-        <h1 className='text-lg font-semibold'>{sum}</h1>
+        <h1 className="text-lg font-semibold">{prop.data.score}</h1>
       </div>
     </div>
   );
