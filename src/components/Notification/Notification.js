b430744d3ddd0d96c3d936a44cd1d35344c886b2
Cem Tuna Koç <60324303+CemTunna@users.noreply.github.com>
2023-06-04 00:52:09 +0300

    notificationrank

--- src/components/Notification/Notification.js
+++ src/components/Notification/Notification.js
@@ -1,40 +1,92 @@
 import { Avatar } from '@mui/material';
-import React from 'react';
+import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
+import { setJobPost } from '../../store/jobpost';
+import CloseIcon from '@mui/icons-material/Close';
+import JbRnk from '../JbRnk';
 
-function Notification({ cname, jname, desc, username, followersName, user }) {
+function Notification({ cname, jname, desc, username, post }) {
+  const [showRnk, setShowRnk] = useState(false);
+
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   return (
     <div className='p-4 bg-white mb-2 rounded-lg flex gap-x-2  items-start  '>
-      <div className='flex pt-1'>
-        <Avatar />
-      </div>
-      {!user && (
-        <div className='w-full flex flex-col gap-y-2'>
-          <p className='text-sm'>
-            <span className='font-semibold'> {cname || ''} </span> is hiring{' '}
-            <span className='font-semibold'> {jname || ''} </span>
-            <br />
-            {desc || ''}
-          </p>
-          <div>
-            <button
-              onClick={() => navigate(`/profile/${username}`)}
-              className='px-3 py-1 border rounded-full hover:bg-slate-400'
-            >
-              See this job
-            </button>
+      {!post && (
+        <>
+          {' '}
+          <div className='flex pt-1'>
+            <Avatar />
           </div>
-        </div>
+          <div className='w-full flex flex-col gap-y-2'>
+            <p className='text-sm'>
+              <span className='font-semibold'> {cname || ''} </span> is hiring{' '}
+              <span className='font-semibold'> {jname || ''} </span>
+              <br />
+              {desc || ''}
+            </p>
+            <div>
+              <button
+                onClick={() => navigate(`/profile/${username}`)}
+                className='px-3 py-1 border rounded-full hover:bg-slate-400'
+              >
+                See this job
+              </button>
+            </div>
+          </div>
+        </>
       )}
-      {user && user.type === 2 && (
-        <div className='w-full flex flex-col gap-y-2'>
-          <p className='text-sm'>
-            <span className='font-semibold'> {followersName || ''} </span> is
-            started to following you <br />
-          </p>
-        </div>
+      {post && post.candidates && post?.candidates?.length > 0 && (
+        <>
+          <div className='w-full flex flex-col  '>
+            <div className='w-full flex flex-col gap-y-2'>
+              <p className='text-sm'>
+                <span className='font-semibold'>
+                  {(post && post?.candidates && post?.candidates?.length) || ''}{' '}
+                  people
+                </span>{' '}
+                is applied for
+                <span className='font-semibold'> {post.jobname || ''}</span>
+                <br />
+              </p>
+              <div>
+                <button
+                  onClick={() => {
+                    dispatch(setJobPost(post));
+                    setShowRnk(true);
+                  }}
+                  className='px-3 py-1 border rounded-full hover:bg-slate-400'
+                >
+                  See who applied
+                </button>
+              </div>
+            </div>
+          </div>
+
+          {showRnk && (
+            <div>
+              <div className='fixed top-0 left-0 bottom-0 right-0 z-50  bg-slate-900 opacity-75'></div>
+              <div className='fixed flex flex-col  z-50 top-5 left-0 right-0 mx-auto max-w-xl max-h-[556px] px-4 py-3 rounded bg-white'>
+                <div className='flex flex-col items-center border-b-2 relative'>
+                  <h1 className=' text-lg p-2 font-medium'>
+                    Candidates Ranking Results
+                  </h1>
+                  <button
+                    onClick={() => setShowRnk(false)}
+                    className='absolute hover:bg-slate-400 rounded-lg p-1 right-0'
+                  >
+                    <CloseIcon />
+                  </button>
+                </div>
+                <div className='flex flex-col gap-y-3 py-3 overflow-y-auto'>
+                  {<JbRnk />}
+                </div>
+              </div>
+            </div>
+          )}
+        </>
       )}
     </div>
   );
--- src/components/Notification/NotificationFeed.js
+++ src/components/Notification/NotificationFeed.js
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import Notification from './Notification';
 import { useSelector } from 'react-redux';
-import { getCompanyInfo } from '../../firebase';
+import { getCompanyInfo, getAllPostbyname } from '../../firebase';
 
 function NotificationFeed() {
   const [companies, setCompanies] = useState([]);
+  const [jobposts, setJobPosts] = useState([]);
 
   const companyUsernames = useSelector((state) => state.auth.user.following);
   const userType = useSelector((state) => state.auth.user.type);
@@ -36,10 +37,23 @@ function NotificationFeed() {
     );
   };
   const handleComp = () => {
-    return user.followers?.map((p) => (
-      <Notification followersName={p} user={user} />
-    ));
+    return (
+      jobposts &&
+      jobposts.length > 0 &&
+      jobposts?.map((p) => <Notification post={p} />)
+    );
   };
+  useEffect(() => {
+    const getPosts = async () => {
+      const posts = await getAllPostbyname(user);
+      const posts2 =
+        posts && posts.length > 0 && posts.filter((p) => p.type === 3);
+
+      setJobPosts(posts2);
+    };
+    getPosts();
+  }, [user]);
+
   return (
     <div className='flex-[0.6] flex-col md:mx-12'>
       {userType === 1 && handleJob()}
